6bdce4e9c9858792f579b07f98848594d9836237
John Silkey <John Silkey>
2017-05-20 11:14:15 -0700

    initialized a firebase

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAvtuzDqSlZpMovTcdiEo2apokzdvo34OI",
+	authDomain: "employee-data-management-aa852.firebaseapp.com",
+	databaseURL: "https://employee-data-management-aa852.firebaseio.com",
+	projectId: "employee-data-management-aa852",
+	storageBucket: "employee-data-management-aa852.appspot.com",
+	messagingSenderId: "349290059912"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 
 
 
@@ -6,9 +19,10 @@ $('#submit').on('click', function(event){
 	event.preventDefault();
 
 	var employeeName = $('#employee-name').val().trim();
-	var startDate = $('#start-date').val().trim();
 	var role = $('#role').val().trim();
+	var startDate = $('#start-date').val().trim();
 	var monthlyRate = $('#monthly-rate').val();
 
-	database.ref()
+	// database.ref()
 });
+
